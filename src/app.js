10006770255219473e0c1a10026b545fecbaf5b9
kevincas01 <kcapupp@princeton.edu>
2022-12-12 22:22:21 -0500

    Stars implemented

--- src/app.js
+++ src/app.js
@@ -6,20 +6,51 @@
  * handles window resizes.
  *
  */
-import { BoxBufferGeometry, Mesh, WebGLRenderer, PerspectiveCamera, Vector3, Scene, Color, MeshBasicMaterial } from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { SeedScene } from 'scenes';
+import { BoxBufferGeometry, Mesh, WebGLRenderer, PerspectiveCamera, Vector3, Scene, Color, MeshBasicMaterial ,BufferGeometry, TextureLoader,PointsMaterial, Points} from 'three';
+
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
 // import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls';
 
 // Initialize core ThreeJS components
 const scene = new Scene();
-scene.background = new Color(0x7ec0ee);
+scene.background = new Color(0x000000);
 const geom = new BoxBufferGeometry(2,2,2);
 const mat = new MeshBasicMaterial();
 const cube = new Mesh(geom, mat);
 
 scene.add(cube);
+
+let points=[]
+
+for (let num = 0; num < 3000; num++) {
+  let coords=new Vector3(Math.ceil(Math.random()*600-300),Math.ceil(Math.random()*600-300),Math.ceil(Math.random()*600-300));
+  
+  points.push(coords)
+}
+let starCoords=  new BufferGeometry().setFromPoints( points )
+
+let texture=new TextureLoader().load('src/components/stars/red.png')
+
+let starMats=new PointsMaterial({size:5,map:texture})
+let brightStars=new Points(starCoords,starMats)
+
+scene.add(brightStars)
+for (let num = 0; num < 3000; num++) {
+  let coords=new Vector3(Math.ceil(Math.random()*600-300),Math.ceil(Math.random()*600-300),Math.ceil(Math.random()*600-300));
+  
+  points.push(coords)
+}
+ starCoords=  new BufferGeometry().setFromPoints( points )
+
+ texture=new TextureLoader().load('src/components/stars/white.png')
+
+ starMats=new PointsMaterial({size:5,map:texture})
+ brightStars=new Points(starCoords,starMats)
+
+scene.add(brightStars)
+
+
+
 
 const camera = new PerspectiveCamera();
 const renderer = new WebGLRenderer({ antialias: true });
